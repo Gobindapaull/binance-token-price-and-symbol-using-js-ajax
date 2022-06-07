8ceca79e8930f0d216e4bb660d1c55c08ad95e17
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-06-07 16:00:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ function getData() {
     xhr.open('GET', "https://api.binance.com/api/v3/ticker/price");
     xhr.send();
 }
-function sendData() {
-
-}
 const getButton = document.getElementById('get');
 
 getButton.addEventListener('click', getData);
